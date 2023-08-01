24941da9c65451a12aa03d390154e9f345c9f3da
codynego <emonenaabednego@gmail.com>
2023-08-01 15:27:31 -0700

    added a customer and srevice_transaction model

--- customer_management_app/models.py
+++ customer_management_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Service_transaction(models.Model):
+    SERVICE_TYPE = {
+        "passport": "passport",
+        "photoshoot": "photoshoot",
+        "photo_printing": "photo_printing"
+    }
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
